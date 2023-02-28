8b7cff394376845ace6807303c94115dd8120f85
marifedt-w <57739727+marifedt@users.noreply.github.com>
2023-02-28 18:45:33 -0500

    Implement validation for sign up form

--- server.js
+++ server.js
@@ -69,7 +69,6 @@ app.post('/login', (req, res) => {
   }
   if (typeof loginPassword !== 'string' || loginPassword.trim().length === 0) {
     passedValidation = false;
-
     validationMessages.password = 'Input your password';
   }
 
@@ -79,6 +78,60 @@ app.post('/login', (req, res) => {
     });
   } else {
     res.render('log-in', {
+      styles: [{ name: 'index.css' }, { name: 'form.css' }],
+      validationMessages,
+      values: req.body,
+    });
+  }
+});
+
+app.post('/sign-up', (req, res) => {
+  const { su_firstName, su_lastName, su_email, su_password } = req.body;
+  let passedValidation = true;
+  let validationMessages = {};
+  let passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^\da-zA-Z]).{8,12}$/;
+  //Reference: https://gist.github.com/frizbee/5318c77d2084fa75cd00ea131399581a
+  //Did some few tweaks to accept any special characters
+  let emailRegex =
+    /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
+  //Source: https://www.w3resource.com/javascript/form/email-validation.php
+
+  //First Name
+  if (typeof su_firstName !== 'string' || su_firstName.trim().length === 0) {
+    passedValidation = false;
+    validationMessages.firstName = 'You must specify your first name';
+  }
+  //Last Name
+  if (typeof su_lastName !== 'string' || su_lastName.trim().length === 0) {
+    passedValidation = false;
+    validationMessages.lastName = 'You must specify your last name';
+  }
+  //Validation for email address
+  if (typeof su_email !== 'string' || su_email.trim().length === 0) {
+    passedValidation = false;
+    validationMessages.email = 'You must enter your email address';
+  } else if (!emailRegex.test(su_email.trim())) {
+    passedValidation = false;
+    validationMessages.email = 'Enter a valid email address';
+  }
+  //Password
+  if (typeof su_password !== 'string' || su_password.trim().length === 0) {
+    passedValidation = false;
+    validationMessages.password = 'You must specify a password';
+  } else if (su_password.trim().length < 8 || su_password.trim().length > 12) {
+    passedValidation = false;
+    validationMessages.password = 'Password should be between 8 to 12 characters long.';
+  } else if (!passwordRegex.test(su_password)) {
+    passedValidation = false;
+    validationMessages.password = 'Password should have at least one lowercase letter, uppercase letter, number and a symbol';
+  }
+
+  if (passedValidation) {
+    res.render('sign-up', {
+      styles: [{ name: 'index.css' }, { name: 'form.css' }],
+    });
+  } else {
+    res.render('sign-up', {
       styles: [{ name: 'index.css' }, { name: 'form.css' }],
       validationMessages,
       values: req.body,
